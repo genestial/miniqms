ff7e202d1bfcbb41e2fc000df7b4878ab783b028
unknown <genestial@gmail.com>
2026-01-30 11:38:38 +0200

    Fix getSignedUrl naming conflict in storage.ts

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,5 +1,5 @@
 import { S3Client, PutObjectCommand, DeleteObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3'
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { getSignedUrl as getS3SignedUrl } from '@aws-sdk/s3-request-presigner'
 import { put as vercelPut, del as vercelDel, list as vercelList } from '@vercel/blob'
 
 // Check if using Vercel Blob
